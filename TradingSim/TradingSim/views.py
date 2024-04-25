55f3e0b4e286095aff5360436b68ae2253b941f2
33QWERTY33 <120336644+33QWERTY33@users.noreply.github.com>
2024-04-24 20:01:14 -0400

    fixed ETF's not having currentPrice data

--- TradingSim/TradingSim/views.py
+++ TradingSim/TradingSim/views.py
@@ -16,10 +16,18 @@ def display_ticker_info(request):
     if len(current_info) < 3: 
         return render(request, "incorrect-ticker.html")
     
+    try:
+        current_info["currentPrice"]
+    except KeyError:
+        current_info["currentPrice"] = current_info["navPrice"]
+    
     for key in keys_used:
         try:
            current_info[key]
         except KeyError:
             current_info[key] = "Not found"
+    
+
+    print(ticker_obj.get_news())
 
     return render(request, "display-ticker-info.html", {"data": current_info})
