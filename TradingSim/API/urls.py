a7dfe4847b19ef0276c7b0c0803985f022ac2d05
33QWERTY33 <120336644+33QWERTY33@users.noreply.github.com>
2024-05-04 00:40:31 -0400

    added CRUD operations for User Portfolio, Buy Orders, and Sell Orders

--- TradingSim/API/urls.py
+++ TradingSim/API/urls.py
@@ -5,12 +5,12 @@ from rest_framework.urlpatterns import format_suffix_patterns
 app_name = "api"
 
 urlpatterns = [
-    path("get-buy-orders", buy_orders),
-    path("get-sell-orders", sell_orders),
-    path("get-portfolios", user_portfolios),
-    path("buy-order/<int:id>", buy_order_by_id),
-    path("sell-order/<int:id>", sell_order_by_id),
-    path("portfolio/<str:user>", portfolio_by_username),
+    path("CR-buy-orders", buy_orders),
+    path("CR-sell-orders", sell_orders),
+    path("CR-portfolios", user_portfolios),
+    path("RUD-buy-order/<int:id>", buy_order_by_id),
+    path("RUD-sell-order/<int:id>", sell_order_by_id),
+    path("RUD-portfolio/<str:user>", portfolio_by_username),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- TradingSim/API/views.py
+++ TradingSim/API/views.py
@@ -1,48 +1,111 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 from UsersApp.models import UserPortfolio
 from OrdersApp.models import BuyOrders, SellOrders
 from .serializers import BuyOrderSerializer, SellOrderSerializer, UserPortfolioSerializer
 
 # Create your views here.
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def buy_orders(request, format=None):
-    orders = BuyOrders.objects.all()
-    serializer = BuyOrderSerializer(orders, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        orders = BuyOrders.objects.all()
+        serializer = BuyOrderSerializer(orders, many=True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        serializer = BuyOrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(["GET"])
+
+@api_view(["GET", "POST"])
 def sell_orders(request, format=None):
-    orders = SellOrders.objects.all()
-    serializer = SellOrderSerializer(orders, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        orders = SellOrders.objects.all()
+        serializer = SellOrderSerializer(orders, many=True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        serializer = SellOrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def user_portfolios(request, format=None):
-    portfolios = UserPortfolio.objects.all()
-    serializer = UserPortfolioSerializer(portfolios, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        portfolios = UserPortfolio.objects.all()
+        serializer = UserPortfolioSerializer(portfolios, many=True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        serializer = UserPortfolioSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(["GET"])
+@api_view(["GET", "PUT", "DELETE"])
 def buy_order_by_id(request, id, format=None):
-    orders = BuyOrders.objects.get(id=id)
-    serializer = BuyOrderSerializer(orders, many=False)
-    print("#"*100, serializer)
-    return Response(serializer.data)
+    try:
+        order = BuyOrders.objects.get(id=id)
+    except BuyOrders.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-@api_view(["GET"])
+    if request.method == "GET":
+        serializer = BuyOrderSerializer(order)
+        return Response(serializer.data)
+    
+    elif request.method == "PUT":
+        serializer = BuyOrderSerializer(order, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+@api_view(["GET", "PUT", "DELETE"])
 def sell_order_by_id(request, id, format=None):
-    orders = SellOrders.objects.get(id=id)
-    serializer = SellOrderSerializer(orders, many=False)
-    return Response(serializer.data)
+    try:
+        order = SellOrders.objects.get(id=id)
+    except SellOrders.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        serializer = SellOrderSerializer(order)
+        return Response(serializer.data)
+    
+    elif request.method == "PUT":
+        serializer = BuyOrderSerializer(order, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["GET"])
+    elif request.method == "DELETE":
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+@api_view(["GET", "PUT", "DELETE"])
 def portfolio_by_username(request, user, format=None):
-    portfolios = UserPortfolio.objects.get(username=user)
-    serializer = UserPortfolioSerializer(portfolios, many=False)
-    return Response(serializer.data)
+    try:
+        portfolio = UserPortfolio.objects.get(username=user)
+    except UserPortfolio.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        serializer = UserPortfolioSerializer(portfolio)
+        return Response(serializer.data)
+    
+    elif request.method == "PUT":
+        serializer = BuyOrderSerializer(portfolio, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-
-
-# NOTE: researching djangorestframework module.
+    elif request.method == "DELETE":
+        portfolio.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
